36c5bce222f8a2512dd58c7b18eaa5b36b950f91
boyboy0710 <boyboy0710@naver.com>
2021-03-03 16:56:49 +0900

    1master

--- python-test.py
+++ python-test.py
@@ -1,3 +1,10 @@
-print("test")
-watch = 100000
-print(watch)
+import pygame
+
+pygame.init()
+
+screen_width = 480
+screen_height = 640
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+pygame.display.set_caption("Game")
+
